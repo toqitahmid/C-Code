bc654b24d1986b2e25791ed10847ab3052ea002e
toqitahmid <rubaiathali@gmail.com>
2024-10-26 09:17:44 +0600

    Add new paragraph

---
+++ Hello.c
@@ -0,0 +1,15 @@
+
+// Write a program that prints a message
+
+#include<stdio.h>
+int main()
+
+{
+    int number = 2;
+
+    printf("Asslamualaikum, everyone");
+    printf("This is my %d nd repositore", number);
+
+    return 0;
+    
+}
